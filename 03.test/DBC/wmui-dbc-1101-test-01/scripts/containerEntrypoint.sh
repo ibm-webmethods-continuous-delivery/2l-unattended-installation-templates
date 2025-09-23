14e7735a99b68f906ffb1a520b096205b423cdca
miunibm <175016327+miunibm@users.noreply.github.com>
2025-09-23 14:56:06 +0000

    Added and tested DBC 11.1 template and post install for postgres
    Signed-off-by: miunibm <175016327+miunibm@users.noreply.github.com>

---
+++ 03.test/DBC/wmui-dbc-1101-test-01/scripts/containerEntrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# This scripts sets up the local installation
+
+# shellcheck disable=SC2153
+if [ ! -d "${WMUI_HOME}" ]; then
+    echo "WMUI_HOME variable MUST point to an existing local folder! Current value is ${WMUI_HOME}"
+    exit 1
+fi
+
+# our configuration takes precedence in front of framework defaults, set it before sourcing the framework functions
+if [ ! -d "${WMUI_LOCAL_SCRIPTS_HOME}" ]; then
+    echo "Scripts folder not found: ${WMUI_LOCAL_SCRIPTS_HOME}"
+    exit 2
+fi
+
+cp "${WMUI_INSTALL_INSTALLER_BIN_MOUNT_POINT}" "${WMUI_INSTALL_INSTALLER_BIN}"
+cp "${WMUI_PATCH_UPD_MGR_BOOTSTRAP_BIN_MOUNT_POINT}" "${WMUI_PATCH_UPD_MGR_BOOTSTRAP_BIN}"
+chmod u+x "$WMUI_INSTALL_INSTALLER_BIN" "$WMUI_PATCH_UPD_MGR_BOOTSTRAP_BIN"
+
+# Source framework functions
+# shellcheck source=../../../../01.scripts/commonFunctions.sh
+. "${WMUI_HOME}/01.scripts/commonFunctions.sh" || exit 4
+# shellcheck source=../../../../01.scripts/installation/setupFunctions.sh
+. "${WMUI_HOME}/01.scripts/installation/setupFunctions.sh" || exit 5
+
+# If the DBC installation is not present, do it now
+if [ ! -f "${WMUI_INSTALL_INSTALL_DIR}/common/db/bin/dbConfigurator.sh" ]; then
+    logI "Database configurator is not present, setting up ..."
+    # Parameters - applySetupTemplate
+    # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
+    applySetupTemplate "DBC/1101/full" || exit 6
+fi
+
+createDbComponents(){
+
+    logI "Eventually creating DB components..."
+    sleep 5 # this container is starting too quickly...
+    while  ! portIsReachable2 "${WMUI_DBSERVER_HOSTNAME}" "${WMUI_DBSERVER_PORT}" ; do
+        logI "Waiting for the database to come up, sleeping 5..."
+        sleep 5
+        # TODO: add an maximum retry number
+    done
+    sleep 5 # allow som time to the DB in any case...
+
+    # template specific parameters
+    applyPostSetupTemplate DBC/1101/postgresql-create
+}
+
+createDbComponents
+
+
+logI "Go to http://host.docker.internal:${WMUI_TEST_PORT_PREFIX}80 and check the database content!. Look at the .env file for details!"
+
+logI "Issue docker-compose down -t 20 to close this project!"
+
+tail -f /dev/null
