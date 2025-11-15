4669be7b9a661116734f82ea52763241fea03288
SPINDOX\gianbattista.bonacin <gianbattista.bonacini@spindox.it>
2025-11-15 13:33:45 +0100

    designer docker enrichment
    Signed-off-by: SPINDOX\gianbattista.bonacin <gianbattista.bonacini@spindox.it>

--- 06.container-image-builders-test/DESIGNER/1101/esb-01/local-build-1/runTestLocal.bat
+++ 06.container-image-builders-test/DESIGNER/1101/esb-01/local-build-1/runTestLocal.bat
@@ -3,14 +3,16 @@
 call .\setEnv.bat
 
 md build_context
+md build_context\plugins
 
-copy ..\..\..\..\05.container-image-builders\%INSTALL_TEMPLATE_win%\alpine\Dockerfile build_context\
-copy ..\..\..\..\05.container-image-builders\%INSTALL_TEMPLATE_win%\alpine\*.sh build_context\
+copy ..\..\..\..\..\05.container-image-builders\%INSTALL_TEMPLATE_win%\ubi-min\Dockerfile build_context\
+copy ..\..\..\..\..\05.container-image-builders\%INSTALL_TEMPLATE_win%\ubi-min\*.sh build_context\
 
 COPY "%INSTALLER_BIN%" build_context\installer.bin
 COPY "%SUM_BOOTSTRAP_BIN%" build_context\upd-mgr-bootstrap.bin
 COPY "%PRODUCTS_ZIP%" build_context\products.zip
 COPY "%FIXES_ZIP%" build_context\fixes.zip
+COPY "%PLUGINS_PATH%"\* build_context\plugins\
 
 cd build_context
 
@@ -20,6 +22,8 @@ docker buildx build --build-arg __wm_install_template=%INSTALL_TEMPLATE% --build
 
 cd ..
 
-del /q build_context\*
+del /q build_context\plugins\*
+rd /q build_context\plugins
 
+del /q build_context\*
 rd /q build_context
