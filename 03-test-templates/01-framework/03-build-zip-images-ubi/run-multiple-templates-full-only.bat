927a988439873624c517a22f91bebf30dd3a050b
miunibm <175016327+miunibm@users.noreply.github.com>
2026-02-17 13:35:44 +0000

    Reverted temporary flag for multiple templates single zip test
    Signed-off-by: miunibm <175016327+miunibm@users.noreply.github.com>

--- 03-test-templates/01-framework/03-build-zip-images-ubi/run-multiple-templates-full-only.bat
+++ 03-test-templates/01-framework/03-build-zip-images-ubi/run-multiple-templates-full-only.bat
@@ -1,4 +1,4 @@
-SET WMUI_TEST_Templates=dbc/1101/full msr/1101/simple
-SET WMUI_TEST_UNION_ZIPS_ONLY=false
+SET WMUI_TEST_Templates=dbc/1101/full msr/1101/sel-25924 agw/1101/cds-e2e
+SET WMUI_TEST_UNION_ZIPS_ONLY=true
 docker compose run --rm wmui-zip-img-builder
 docker compose down -t 0 -v
