3b0d91128e8802c7980ac3b27c780b007f968341
miunibm <175016327+miunibm@users.noreply.github.com>
2026-03-04 11:32:47 +0000

    Test shows defaults are required when building images
    Signed-off-by: miunibm <175016327+miunibm@users.noreply.github.com>

---
+++ 02-templates/01-setup/designer/1101/esb-01/01-set-env-defaults.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export WMUI_WMSCRIPT_HostName="${WMUI_WMSCRIPT_HostName:-localhost}"
+export WMUI_WMSCRIPT_InstallDir="${WMUI_WMSCRIPT_InstallDir:-/opt/webmethods}"
+export WMUI_WMSCRIPT_adminPassword="${WMUI_WMSCRIPT_adminPassword:-manage}"
